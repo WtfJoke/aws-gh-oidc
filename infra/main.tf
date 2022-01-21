0094ab981edccdaaae7d282a82027decf4a4c919
Manuel <manuel.wessner@codecentric.de>
2022-01-21 16:32:31 +0100

    :bricks: Print hello :wave: from aws

--- infra/main.tf
+++ infra/main.tf
@@ -15,8 +15,20 @@ provider "aws" {
   }
 }
 
-resource "aws_ssm_parameter" "sample" {
-  name  = "sample_${local.project_name}"
+resource "aws_ssm_parameter" "hello" {
+  name  = "hello_${local.project_name}"
   type  = "String"
-  value = "Hi"
+  value = "Hi from aws :wave:"
+}
+
+resource "aws_iam_openid_connect_provider" "aws-oidc-github-identity-provider" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+  # Obtained from the certificate of the url, see here for more details https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_providers_create_oidc_verify-thumbprint.html
+  # Alternatively this can also be obtained by the aws managagement console when creading an oidc provider and use the "Get Thumbprint button"
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
---
+++ infra/policies.tf
@@ -0,0 +1,50 @@
+resource "aws_iam_role" "github-actions-role" {
+  name               = "aws-gh-oidc"
+  description        = "Role to be assumed for a demonstration in https://github.com/WtfJoke/aws-gh-oidc"
+  assume_role_policy = data.aws_iam_policy_document.github-actions-assume-role-trust-policy.json
+}
+
+data "aws_iam_policy_document" "github-actions-assume-role-trust-policy" {
+
+  // Trust Policy
+  statement {
+    sid     = "GithubActionsAssumeRoleWithIdp"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.aws-oidc-github-identity-provider.arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        "repo:WtfJoke/aws-gh-oidc:ref:refs/heads/main",
+        "repo:WtfJoke/aws-gh-oidc:ref:refs/heads/hello"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "github-actions-role-hello-attach" {
+  role       = aws_iam_role.github-actions-role.name
+  policy_arn = aws_iam_policy.read-hello-parameter-policy.arn
+}
+
+resource "aws_iam_policy" "read-hello-parameter-policy" {
+  name        = "${local.project_name}-allow-read-hello-parameter-policy"
+  description = "Allows to read ${aws_ssm_parameter.hello.name} parameter"
+
+  policy = data.aws_iam_policy_document.read-hello-parameter-policy-document.json
+}
+
+data "aws_iam_policy_document" "read-hello-parameter-policy-document" {
+
+  // Permission Policy
+  // Allow to read ssm parameter hello
+  statement {
+    sid       = "AllowReadHello"
+    actions   = ["ssm:GetParameters", "ssm:GetParameter"]
+    resources = [aws_ssm_parameter.hello.arn]
+  }
+}
